afff5e89b267d5d293cf2dc86e327a813ed2ab57
Kieve Chua <kievechua@gmail.com>
2017-05-05 10:11:50 +0800

    Use modelName config

---
+++ dist/config/umzug.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    migration: {
+        path: 'src/database/migrations',
+        up: false,
+        down: false
+    },
+    seeder: {
+        path: 'src/database/seeders',
+        up: false,
+        down: false
+    }
+};
+//# sourceMappingURL=umzug.js.map
